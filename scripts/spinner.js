83d2124d2512350edbb0f6eb462116c2ef3096bb
Aras ARKAN <arasarkan9@gmail.com>
2024-09-02 13:32:18 +0300

    test

---
+++ scripts/spinner.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var overlay = document.getElementById("overlay");
+    setTimeout(function() {
+        overlay.style.display = "none";
+    }, 500);
+});
